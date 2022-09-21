583e84e6d9735d57bf668501a047a10601d4071c
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-09-21 15:06:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,12 +8,5 @@ import Notiflix from "notiflix";
 
 const discoveryFetch = new DiscoveryFetch;
 
-<<<<<<< HEAD
 toggleModal();
-=======
 
-<<<<<<< HEAD
->>>>>>> 821e11327a54fd7f0ae753de89b9ae7d74174237
-=======
-
->>>>>>> 821e11327a54fd7f0ae753de89b9ae7d74174237
