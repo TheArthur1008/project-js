f23040a1110c073d5c6238abe7043e87e3e635b6
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-09-21 11:11:29 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,24 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 export class DiscoveryFetch {
   #URL = 'https://app.ticketmaster.com/discovery/v2/events';
-  #KEY = 'kLVTW7dqtxEqtJSKTwWutczpAe3AhzWs';;
+  #KEY = 'kLVTW7dqtxEqtJSKTwWutczpAe3AhzWs';
   constructor() {
     this.size = 16;
     this.keyword = '';
     this.page = 1;
     this.countryCode = '';
-  } 
+  }
 
   fetchEvents() {
     const options = {
       apikey: this.#KEY,
       keyword: this.keyword,
       size: this.size,
-    page: this.page,
+      page: this.page,
       countryCode: this.countryCode,
     };
     return axios.get(`${this.#URL}`, { params: options });
   }
-
 }
