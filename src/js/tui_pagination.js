55ac0c654eda19785e988741ae72a05df5bead3a
Olexandr Beisiuk <57046480+Bolexandr@users.noreply.github.com>
2022-09-22 20:02:42 +0300

    good_pagination
    good_pagination

--- src/js/tui_pagination.js
+++ src/js/tui_pagination.js
@@ -1,76 +1,55 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-import { DiscoveryFetch } from './api';
-import createEventList from './templates/event-list.hbs';
 
-const discoveryFetch = new DiscoveryFetch();
-const eventList = document.querySelector('.event-list');
+const container = document.getElementById('pagination');
 
-////////Приймає відповідь з апішки в першому then
-function tuiPagination(responseData) {
-  console.log(responseData);
-  const { size, totalElements, totalPages, number } = responseData.page;
-  console.log(size);
-  console.log(totalElements);
-  console.log(totalPages);
-  console.log(number);
+export class TuiPaginationClass {
+  constructor() {
+    this.totalItems = 100;
+    this.itemsPerPage = 16;
+    this.page = 1;
+    this.paginationPageNumber = 0;
+  }
+  itnitializationExem() {
+    const options = {
+      totalItems: this.totalItems,
+      itemsPerPage: this.itemsPerPage,
+      visiblePages: 5,
+      page: this.page,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+    };
+    setTimeout(() => {
+      document.querySelector('.tui-last').innerHTML = `<span>${Math.ceil(
+        this.totalItems / this.itemsPerPage
+      )}</span>`;
+    }, 5);
 
-  const options = {
-    totalItems: totalElements,
-    itemsPerPage: size,
-    visiblePages: 5,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-  };
+    const myPagination = new Pagination(container, options);
+    myPagination.on('beforeMove', evt => {
+      this.paginationPageNumber = evt.page;
 
-  const container = document.getElementById('pagination');
-  const myPagination = new Pagination(container, options);
+      if (
+        this.paginationPageNumber <
+        Math.ceil(this.totalItems / this.itemsPerPage) - 2
+      ) {
+        setTimeout(() => {
+          document.querySelector('.tui-last').innerHTML = `<span>${Math.ceil(
+            this.totalItems / this.itemsPerPage
+          )}</span>`;
+        }, 5);
+      } else {
+        document.querySelector('.tui-last').innerHTML = '';
+      }
 
-  /////Метод повертає сторінку myPagination.on('beforeMove' на яку перейшов користувач:
-  myPagination.on('beforeMove', evt => {
-    const { page } = evt;
-    console.log(evt);
-    discoveryFetch.page = page - 1;
-
-    ///////логіка скривання першої стрілки
-    if (page > 1) {
-      setTimeout(() => {
-        document.querySelector('.tui-first').classList.add('visually-hidden');
-        document.querySelector('.tui-first').innerHTML = '<span>|&#8656</span>';
-      }, 1);
-    }
-
-    //////Логіка скривання  перших двох стрілок
-    if (page >= 4) {
-      setTimeout(() => {
-        document
-          .querySelector('.tui-first')
-          .classList.remove('visually-hidden');
-        document.querySelector('.tui-prev-is-ellip').innerHTML =
-          '<span>&#8656</span>';
-      }, 2);
-      ///логіка послідньої цифри пропадання і з'являння
-      //   if (Math.trunc(totalPages / 5) === Math.trunc(page / 5)) {
-      //     document.querySelector('.tui-last').classList.add('visually-hidden');
-      //   } else {
-      //     console.log('casa');
-      //     document.querySelector('.tui-last').classList.remove('visually-hidden');
-      //   }
-    }
-    // ////////////////////Логіка виклику запитів
-  });
-
-  // ///// Стрілка вперед:
-  document.querySelector('.tui-last-child').innerHTML = '<span>&#8658</span>';
-
-  ///// вказівник послідньої сторінки
-  document.querySelector('.tui-last').innerHTML = `<span>${totalPages}</span>`;
+      if (evt.page > 3) {
+        setTimeout(() => {
+          document.querySelector('.tui-first').innerHTML = '<span>1</span>';
+        }, 5);
+      } else {
+        document.querySelector('.tui-first').innerHTML = '';
+      }
+    });
+  }
 }
-
-const onLoadMore = currentPage => {
-  discoveryFetch.page = currentPage;
-};
-
-// export default
